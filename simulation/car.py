4b6491a25bc752dfce7cd90f4fdc00db66a412fc
Ole Andreas Hansen <oleaha@me.com>
2018-02-02 10:00:47 +0100

    Planner

--- simulation/car.py
+++ simulation/car.py
@@ -3,7 +3,9 @@ from simulation.location.location import Location
 from simulation.piborg.motorControlMock import MotorControl
 from simulation.planner.planner import Planner
 
+import logging
 import time
+
 
 class Car:
 
@@ -20,19 +22,19 @@ class Car:
         self.car['prev_pos'] = pos
         self.car['to_dir'] = to_dir
         self.car['from_dir'] = from_dir
-
+        # Thread logger
+        logging.basicConfig(level=logging.DEBUG, format='%(relativeCreated)6d %(threadName)s %(message)s')
+        logging.debug("car.py started")
 
         # Initialize location module
         self.LOC = Location(self.car['curr_pos'])
         self.MC = MotorControl()
 
-        self.PLANNER = Planner(1, "Planner", self.LOC)
+        self.PLANNER = Planner(1, "Planner", self.LOC, self)
         self.PLANNER.start()
 
-        print("Hmmm")
-
-
-        self.LOC.map.print_map(self.car['curr_pos'])
+        time.sleep(5)
+        self.PLANNER.stop_thread()
 
 
 c = Car('192.168.1.1.', (12, 24), 'e', 'w')
--- simulation/planner/planner.py
+++ simulation/planner/planner.py
@@ -1,25 +1,31 @@
-import time
 import threading
 from simulation.location.location import Location
+from simulation.car import Car
+from simulation.utils.direction import Direction
+from simulation import settings
+import logging
+import random
 
 
 class Planner(threading.Thread):
 
-    def __init__(self, threadId, name, location):
+    def __init__(self, threadId, name, location, car):
         """
         :type location: Location
+        :type car: Car
         """
         threading.Thread.__init__(self)
         self.threadId = threadId
         self.name = name
         self.exitFlag = False
         self.route = []
-        self.location = location
+        self.location = location  # Passed from car.py
+        self.car = car.car
 
     def run(self):
-        print "Starting thread: " + self.name
+        logging.debug("thread started")
         self.planner()
-        print "Exiting thread: " + self.name
+        logging.debug("thread stopped")
 
     def planner(self):
         while not self.exitFlag:
@@ -27,9 +33,36 @@ class Planner(threading.Thread):
                 self.calculate_next_step()
 
     def calculate_next_step(self):
-        self.route.append(self.location.get_car_pos())
-        print(self.location.get_car_pos())
 
+        if self.location.in_intersection(self.location.get_car_pos()):
+            prob = random.uniform(0, 1)
+            if self.car['to_dir'] == Direction.SOUTH:
+                self._calculate_turn(prob, Direction.WEST, Direction.EAST, Direction.SOUTH)
+            elif self.car['to_dir'] == Direction.NORTH:
+                self._calculate_turn(prob, Direction.WEST, Direction.EAST, Direction.NORTH)
+            elif self.car['to_dir'] == Direction.WEST:
+                self._calculate_turn(prob, Direction.NORTH, Direction.SOUTH, Direction.WEST)
+            else:
+                self._calculate_turn(prob, Direction.NORTH, Direction.SOUTH, Direction.EAST)
+        else:
+            self.drive_straight(self.car['to_dir'])
+
+        self.route.append(self.location.get_car_pos())
+        logging.debug(self.location.get_car_pos())
+
+    def _calculate_turn(self, prob, left, right, straight):
+        if prob <= settings.PROBABILITIES['left']:
+            self.quarter_turn(left)
+        elif prob <= settings.PROBABILITIES['right']:
+            self.quarter_turn(right)
+        else:
+            self.drive_straight(straight)
+
+    def quarter_turn(self, direction):
+        pass
+
+    def drive_straight(self, direction):
+        pass
 
     def stop_thread(self):
         self.exitFlag = True
