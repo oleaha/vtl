5f0dcc3668d50d64415c91d6ab940ab462111aaf
Ole Andreas Hansen <oleaha@me.com>
2018-02-21 13:08:52 +0100

    Update motor control module to support DiddyBorg V2

--- simulation/piborg/motorControl.py
+++ simulation/piborg/motorControl.py
@@ -1,9 +1,91 @@
 import PicoBorgRev
 import time
 import sys
+from simulation.piborg import ThunderBorg
 
 
-class MotorControl(object):
+class MotorControlV2:
+    """
+    Class for DiddyBorg V2, requires ThunderBorg.py
+    """
+
+    def __init__(self):
+        # Set up thunderborg
+        self.TB = ThunderBorg.ThunderBorg()
+        self.TB.Init()
+
+        if not self.TB.foundChip:
+            boards = ThunderBorg.ScanForThunderBorg()
+
+            if len(boards) == 0:
+                print "No ThunderBorg found, check that you are attached :)"
+            else:
+                print "No ThunderBorg at address %02X, but we did find boards:" % self.TB.i2cAddress
+                for board in boards:
+                    print '     %02X (%d)' % (board, board)
+                print "You need to change the I2C address. Change the setup line so it is correct"
+                print "TB.i2cAddress = 0x%02X" % (boards[0])
+            sys.exit()
+
+        self.TB.SetCommsFailsafe(False)
+        self.timeForwardOneMeter = 5.2
+
+        self.timeSpinThreeSixty = 5.2
+        self.voltageIn = 12.0
+        self.voltageOut = 0.95 * 12
+
+        if self.voltageOut > self.voltageIn:
+            self.maxPower = 1.0
+        else:
+            self.maxPower = self.voltageOut / float(self.voltageIn)
+
+    def perform_move(self, drive_left, drive_right, num_seconds):
+        self.TB.SetMotor1(drive_left * self.maxPower)
+        self.TB.SetMotor2(drive_right * self.maxPower)
+
+        time.sleep(num_seconds)
+        self.TB.MotorsOff()
+
+    def perform_spin(self, angle):
+        if angle < 0.0:
+            # Left turn
+            drive_left = -1.0
+            drive_right = 1.0
+            angle *= -1
+        else:
+            # Right turn
+            drive_left = 1.0
+            drive_right = -1.0
+
+        num_seconds = (angle / 360.0) * self.timeSpinThreeSixty
+
+        self.perform_move(drive_left, drive_right, num_seconds)
+
+    def perform_drive(self, meters):
+        if meters < 0.0:
+            drive_left = -1.0
+            drive_right = -1.0
+        else:
+            drive_left = 1.0
+            drive_right = 1.0
+
+        num_seconds = meters * self.timeForwardOneMeter
+        self.perform_move(drive_left, drive_right, num_seconds)
+
+    def set_led_green(self):
+        self.TB.SetLeds(0, 1, 0)
+
+    def set_led_red(self):
+        self.TB.SetLeds(1, 0, 0)
+
+    def set_led_yellow(self):
+        self.TB.SetLeds(1, 0.5, 0)
+
+
+class MotorControlV1(object):
+    """
+    Class for DiddyBorg V1, requires PicoBorgRev.py
+    """
 
     def __init__(self):
         # Set up PicoBorg Reverse
