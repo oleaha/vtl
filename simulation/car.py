5301183acfb87dea1ac2c6098636c12d1b586d42
Ole Andreas Hansen <oleaha@me.com>
2018-05-05 16:02:34 +0200

    Make sure all cars are in location table on startup

--- simulation/car.py
+++ simulation/car.py
@@ -65,7 +65,7 @@ class Car:
         try:
             start = time.time()
             while True:
-                if self.plan.qsize() > 5 and len(self.location_table) > 2:
+                if self.plan.qsize() > 5 and len(self.location_table) == 3:
                     self.execute_command()
                     # TODO: This needs to be tested more
                     """
