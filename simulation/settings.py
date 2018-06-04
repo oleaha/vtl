018c1f8db6f31438f47ba73f95ab5c3aec525ec6
Ole Andreas Hansen <oleaha@me.com>
2018-06-04 10:16:59 +0200

    Cleanup

--- simulation/settings.py
+++ simulation/settings.py
@@ -1,23 +1,9 @@
 
-
 """
-Map layout constants. Size of map in X and Y direction.
-Definition of roads and intersections
-"""
-
-"""
-# Large map
-MAP_SIZE_X = 61
-MAP_SIZE_Y = 40
-ROADS_X = [19, 20]
-ROADS_Y = [19, 20, 40, 41]
-INTERSECTIONS = [
-    ((19, 19), (19, 20), (20, 19), (20, 20)),
-    ((19, 40), (19, 41), (20, 40), (20, 41))
-]
+Define the map layout constants to build the map. 
+Number of cells in X and Y direction. Which cells are roads and where intersections are to be placed
 """
 
-# Small map
 MAP_SIZE_X = 12
 MAP_SIZE_Y = 10
 ROADS_X = [4, 5]
@@ -27,8 +13,17 @@ INTERSECTIONS = [
     ((4, 7), (4, 8), (5, 7), (5, 8))]
 
 
+"""
+Define the probabilities of performing a turn in an intersection. 
+P(left) = 0.3
+P(right) = 0.3
+P(straight) = 1 - P(left) - P(right)
+"""
 PROBABILITIES = {'left': 0.30, 'right': 0.60}
 
+"""
+Constants used for ThunderBorg to calibrate the robots movement
+"""
 QUARTER_TURN_DEGREES = 90
 HALF_TURN_DEGREES = 180
 DRIVE_STEP = 0.25  # 20 centimeters per step
@@ -52,19 +47,21 @@ MULTICAST_GROUP_IP = "224.3.29.71"
 MULTICAST_PORT = 10000
 
 
-# Lane detection settings
+"""
+Lane detection settings describing frame rate, resulution and the center point in the image 
+"""
 CAMERA_FRAME_RATE = 3
 CAMERA_RESOLUTION = (752, 480)
 CAMERA_VFLIP = False
 CAMERA_HFLIP = False
 CAMERA_WARMUP_TIME = 0.1
-
 ACTUAL_CENTER = 383  # in pixels
 LANE_DEBUG = False
 
-
-# Traffic light
-TRAFFIC_LIGHT_INTERVAL = 12  # Seconds
-TRAFFIC_LIGHT_YELLOW_DURATION = 3  # Seconds
+"""
+Definition of a traffic light cycle in seconds
+"""
+TRAFFIC_LIGHT_INTERVAL = 12
+TRAFFIC_LIGHT_YELLOW_DURATION = 3
 CAR_IS_PRIMARY = True
 TRAFFIC_LIGHT_BROADCAST_PORT = 5555
